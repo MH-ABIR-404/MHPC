af8e59771e20d638faf4b76e39975d3e0277280c
MOHAMMAD ABIR HOSSAIN <abir232421@gmail.com>
2022-11-18 23:28:03 +0600

    Add files via upload

---
+++ Public.py
@@ -0,0 +1,7 @@
+import time
+print("""[1;91m[>] Encoded By MH-ABIR-404
+[m[>] Github :MH-ABIR-404
+[1;91m[>] Encryption Type : Py3 Marshal[3.11]
+[m-----------------------------------------""")
+time.sleep(0.02)
+import marshal
